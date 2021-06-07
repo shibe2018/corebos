6b57c0057d5754df7b8fccf047db1991825d3f50
Mohamed.Abdallah <m.shibe@spikeassociates.com>
2021-06-07 10:04:02 +0300

    i18n(GlobalVariable) translate fixed global variable name

--- modules/GlobalVariable/language/en_us.gvdefs.php
+++ modules/GlobalVariable/language/en_us.gvdefs.php
@@ -139,7 +139,7 @@ $GlobalVariableDefinitons = array(
 	'values' => 'All | Last5 | Mine',
 	'definition' => '"All" (default value) will show all comments related to the record, "Last5" will show only the last 5 comments and "Mine" will show all comments of the current user.',
 ),
-'Comments_Export_Format' => array(
+'ModComments_DefaultBlockStatus' => array(
 	'status' => 'Implemented',
 	'valuetype' => 'String',
 	'category' => 'Application',
